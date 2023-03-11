694e018f38caf79d59da2af055259f5acb03c7cd
Chanatinart K <chanatinart.k@gmail.com>
2023-03-11 13:18:20 +0700

    complete part2

---
+++ part2.js
@@ -0,0 +1,21 @@
+
+// Challenge 1: Create an Age Calculator
+const birthOfYear = 1994;
+const futureYear = 2026;
+
+let ageOfFutureYear = futureYear - birthOfYear;
+
+console.log(`If you were born in ${birthOfYear}, then in ${futureYear} you’ll be ${ageOfFutureYear}`)
+
+
+//Challenge 2: Create an information card
+let person = {
+    name:'Gift',
+    location: 'Bangkok',
+    hobby: 'playing games'
+}
+
+let pName = person.name
+let pLocation = person.location
+let pHobby = person.hobby 
+console.log(`Hi, my name is ${pName}. I live in ${pLocation} and enjoy ${pHobby}.`)
